bede2859ed9316079f637ddff56eb1217df9ee36
Enes <kokcamenes@gmail.com>
2025-09-02 22:11:19 +0300

    add ui, cqrs, mediatr

---
+++ Infrastructure/MoviesApi.Persistence/Migrations/20250428191046_mig3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Persistence.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // 1. Duration kolonunu sil
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+
+            // 2. Duration kolonunu int olarak tekrar ekle
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Geri dönerken int kolonu sil
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+
+            // datetime2 olarak tekrar ekle
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Duration",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()");
+        }
+    }
+}
